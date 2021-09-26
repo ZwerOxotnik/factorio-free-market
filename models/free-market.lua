58c19a7e628bc7f09344c60ac2ac7abddfe93f4a
ZwerOxotnik <zweroxotnik@gmail.com>
2021-09-26 17:18:07 +0300

    Update to 0.12.1

--- models/free-market.lua
+++ models/free-market.lua
@@ -68,10 +68,7 @@ local is_public_titles = settings.global["FM_is-public-titles"].value
 ---@param t? table
 ---@return boolean
 local function is_empty(t)
-	for _ in pairs(t) do
-		return false
-	end
-	return true
+	return next(t) == nil
 end
 
 ---@param entity LuaEntity
@@ -1410,22 +1407,23 @@ local function on_gui_click(event)
 end
 
 local function check_buy_boxes()
-	local force_index = mod_data.last_buyer_index
-	for _, index in pairs(active_forces) do
-		if index > force_index then
-			force_index = index
-			break
-		end
-	end
+	next(active_forces, mod_data.last_buyer_index)
+	-- local force_index = mod_data.last_buyer_index
+	-- for _, index in pairs(active_forces) do
+	-- 	if index > force_index then
+	-- 		force_index = index
+	-- 		break
+	-- 	end
+	-- end
 
-	if force_index ~= mod_data.last_buyer_index then
-		mod_data.last_buyer_index = force_index
-	elseif force_index == 1 then
-		return
-	else
-		force_index = 1
-		mod_data.last_buyer_index = 1
-	end
+	-- if force_index ~= mod_data.last_buyer_index then
+	-- 	mod_data.last_buyer_index = force_index
+	-- elseif force_index == 1 then
+	-- 	return
+	-- else
+	-- 	force_index = 1
+	-- 	mod_data.last_buyer_index = 1
+	-- end
 
 	local forces_money = call("EasyAPI", "get_forces_money")
 	local forces_money_copy = {}
