fcae24965ea87cc4daaf42f47d43615812cc7522
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-09 20:48:44 +0300

    Revert "Merge remote-tracking branch 'origin/main' into infinite_storage"
    This reverts commit a0d770e7ab7fe8ee41f2276487011ec7678fc9ce, reversing
    changes made to dcf9755d5078749469b52bd5661cec2704e19640.

--- settings.lua
+++ settings.lua
@@ -1,6 +1,8 @@
 data:extend({
 	{type = "int-setting", name = "FM_money-treshold", setting_type = "runtime-global", default_value = 100, minimal_value = 1, maximal_value = 1e19},
 	{type = "int-setting", name = "FM_update-tick", setting_type = "runtime-global", default_value = 6, minimal_value = 1, maximal_value = 8e4},
+	{type = "int-setting", name = "FM_update-sell-tick", setting_type = "runtime-global", default_value = 120, minimal_value = 2, maximal_value = 8e4},
+	{type = "int-setting", name = "FM_update-pull-tick", setting_type = "runtime-global", default_value = 360, minimal_value = 1, maximal_value = 8e4},
 	{type = "int-setting", name = "FM_minimal-price", setting_type = "runtime-global", default_value = 1, minimal_value = 1, maximal_value = 1e19},
 	{type = "int-setting", name = "FM_maximal-price", setting_type = "runtime-global", default_value = 1e9, minimal_value = 1, maximal_value = 1e19},
 	{type = "int-setting", name = "FM_skip_offline_team_chance", setting_type = "runtime-global", default_value = 0, minimal_value = 0, maximal_value = 100},
